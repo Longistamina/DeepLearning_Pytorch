89b58819706c296a1ef5ef954e15aefe094e9cf0
LongSNU <longdpt1002@snu.ac.kr>
2025-12-22 12:00:59 +0900

    complete file

---
+++ 01_Pytorch_Fundamentals/00_what_is_Pytorch.py
@@ -0,0 +1,12 @@
+'''
+Pytorch is the most popular research deep learning framework.
+
+Write fast deep learning code in Python (able to run on a GPU or many GPUs)
+
+Able to access many pre-built deep learning models (Torch Hub / torch.vision.models)
+
+Whole stack: preprocess data, model data, deploy modle in your application/cloud
+
+Originally designed and used in-house by Facebook/Meta 
+(now open-source and used by many other companies like Tesla, Microsoft, OpenAI)
+'''
