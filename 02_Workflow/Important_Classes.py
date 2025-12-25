efc72570e7e300500cf843bc23a27f0c3b11cfe1
LongSNU <longdpt1002@snu.ac.kr>
2025-12-25 14:11:39 +0900

    added contents

--- 02_Workflow/Important_Classes.py
+++ 02_Workflow/Important_Classes.py
@@ -1,6 +1,12 @@
 '''
 torch.nn - contains all the building blocks for neural networks (computational graphs)
+
 torch.nn.Parameter - what parameters should our model try and learn, often a PyTorch layer from torch.nn will set these for us
+
 torch.nn.Module - the base class for all neural network modules. (Should always subclass this class
                   (if you subclass nn.Module above, then should always overwrite forward())
+                
+torch.optim - contains all the optimizers, helping us with gradient descent
+
+torch.utils.data - contains tools for dataset handling
 '''
