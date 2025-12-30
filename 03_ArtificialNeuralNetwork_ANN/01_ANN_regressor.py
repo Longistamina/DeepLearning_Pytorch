1761e81eef6a61f50910f003bf24e04bc0e8d19a
LongSNU <longdpt1002@snu.ac.kr>
2025-12-30 15:23:28 +0900

    updated contents

--- 03_ArtificialNeuralNetwork_ANN/01_ANN_regressor.py
+++ 03_ArtificialNeuralNetwork_ANN/01_ANN_regressor.py
@@ -38,30 +38,182 @@ print(pl_housing.shape)
 # (20640, 13)
 
 '''----- X ------'''
-X = torch.tensor(
-    data=pl_housing.select(c("*").exclude("median_house_value")).to_numpy(),
+from sklearn.preprocessing import RobustScaler
+
+X_raw = pl_housing.select(c("*").exclude("median_house_value")).to_numpy()
+
+X_scaler = RobustScaler().fit(X_raw)
+
+X_scaled = torch.tensor(
+    data=X_scaler.transform(X_raw),
     device=device
 )
-print(X)
-# tensor([[-122.2300,   37.8800,   41.0000,  ...,    0.0000,    0.0000,
-#             0.0000],
-#         [-122.2200,   37.8600,   21.0000,  ...,    0.0000,    0.0000,
-#             0.0000],
-#         [-122.2400,   37.8500,   52.0000,  ...,    0.0000,    0.0000,
-#             0.0000],
+
+print(X_scaled)
+# tensor([[-0.9868,  0.9577,  0.6316,  ...,  0.0000,  0.0000,  0.0000],
+#         [-0.9842,  0.9524, -0.4211,  ...,  0.0000,  0.0000,  0.0000],
+#         [-0.9894,  0.9497,  1.2105,  ...,  0.0000,  0.0000,  0.0000],
 #         ...,
-#         [-121.2200,   39.4300,   17.0000,  ...,    1.0000,    0.0000,
-#             0.0000],
-#         [-121.3200,   39.4300,   18.0000,  ...,    1.0000,    0.0000,
-#             0.0000],
-#         [-121.2400,   39.3700,   16.0000,  ...,    1.0000,    0.0000,
-#             0.0000]], device='cuda:0')
+#         [-0.7203,  1.3677, -0.6316,  ...,  1.0000,  0.0000,  0.0000],
+#         [-0.7467,  1.3677, -0.5789,  ...,  1.0000,  0.0000,  0.0000],
+#         [-0.7256,  1.3519, -0.6842,  ...,  1.0000,  0.0000,  0.0000]],
+#        device='cuda:0')
+
+print(X_scaled.shape)
+# torch.Size([20640, 12])
+# Here, 12 will be the in_features
 
 '''----- y ------'''
-y = torch.tensor(
-    data=pl_housing.get_column("median_house_value").to_numpy(),
+from sklearn.preprocessing import RobustScaler
+
+y_raw = pl_housing.select("median_house_value").to_numpy()
+
+y_scaler = RobustScaler().fit(y_raw)
+
+y_scaled = torch.tensor(
+    data=y_scaler.transform(y_raw),
     device=device
 )
-print(y)
-# tensor([452600., 358500., 352100.,  ...,  92300.,  84700.,  89400.],
-#        device='cuda:0')
+
+print(y_scaled)
+# tensor([[ 1.8804],
+#         [ 1.2320],
+#         [ 1.1879],
+#         ...,
+#         [-0.6022],
+#         [-0.6546],
+#         [-0.6222]], device='cuda:0')
+
+print(y_scaled.shape)
+# torch.Size([20640, 1])
+# Here, 1 will be the final out_features
+
+#######################
+## Dataset splitting ##
+#######################
+
+train_len = int(0.8 * len(X_scaled)) # MUST be INTEGER
+val_len = int(0.1 * len(X_scaled))
+test_len = len(X_scaled) - (train_len + val_len)
+
+from torch.utils.data import DataLoader, TensorDataset, random_split
+
+full_dataset = TensorDataset(X_scaled, y_scaled)
+train_split, val_split, test_split = random_split(dataset=full_dataset, lengths=[train_len, val_len, test_len])
+
+train_set = DataLoader(train_split, batch_size=2**11, shuffle=True)
+val_set = DataLoader(val_split, batch_size=2**11, shuffle=True)
+test_set = DataLoader(test_split, batch_size=2**11, shuffle=True)
+
+####################
+## Model building ##
+####################
+
+from torch import nn
+
+class ANNmodel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        '''Use nn.Sequential() to put our layers together'''
+        self.main = nn.Sequential(
+            
+            # Layer 1: 12 inputs -> 64 neurons (use nn.ReLU() as activation since median_house_value is non-negative)
+            nn.Linear(in_features=12, out_features=64),
+            nn.BatchNorm1d(64), # Normalize
+            nn.ReLU(),
+            
+            # Layer 2: 64 neurons -> 32 neurons
+            nn.Linear(64, 32),
+            nn.BatchNorm1d(32), # Normalize
+            nn.ReLU(),
+            
+            # Layer 3: 32 neurons -> 1 output (No activation function here because this is regression!)
+            nn.Linear(32, 1)
+        )
+    
+    def forward(self, X):
+        return self.main(X)
+    
+'''Call out a model as an instance of a class'''
+torch.manual_seed(42)
+model = ANNmodel()
+model.to(device)
+
+# print(f'\nParameters before training:\n{model.state_dict()}')
+# .....
+
+########################
+## Loss and Optimizer ##
+########################
+
+loss_fn = nn.SmoothL1Loss()
+
+optimizer = torch.optim.Adam(
+    params=model.parameters(), 
+    lr=1
+)
+
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+
+################################
+## Training - Validating loop ##
+################################
+
+epochs = 100
+
+train_loss_list, val_loss_list = [], []
+
+for epoch in range(1, epochs+1, 1):
+    # --- TRAINING ---
+    _ = model.train()
+    for X_batch, y_batch in train_set:
+        y_preds = model(X_batch)
+        loss = loss_fn(y_preds, y_batch)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    # --- VALIDATION (Every epoch) ---
+    _ = model.eval() # 1. Set model to evaluation mode
+    val_loss = 0
+    with torch.inference_mode(): # 2. Turn off gradient tracking to save memory
+        for X_val, y_val in val_set: # 3. Iterate through val_set
+            val_preds = model(X_val)
+            # Accumulate loss to get an average for the whole set
+            val_loss += loss_fn(val_preds, y_val).item()
+    
+    avg_val_loss = val_loss / len(val_set)
+    
+    train_loss_list.append(loss.item())
+    val_loss_list.append(avg_val_loss)
+    
+    scheduler.step()
+    
+    if epoch % 20 == 0:
+        print("+"*50)
+        print(f"Epoch: {epoch}")
+        print(f"Train loss: {loss:.3e}")
+        print(f"Validation loss: {avg_val_loss:.3e}")
+'''
+++++++++++++++++++++++++++++++++++++++++++++++++++
+Epoch: 20
+Train loss: 1.230e-01
+Validation loss: 1.276e-01
+++++++++++++++++++++++++++++++++++++++++++++++++++
+Epoch: 40
+Train loss: 1.634e-01
+Validation loss: 1.082e-01
+++++++++++++++++++++++++++++++++++++++++++++++++++
+Epoch: 60
+Train loss: 1.553e-01
+Validation loss: 1.064e-01
+++++++++++++++++++++++++++++++++++++++++++++++++++
+Epoch: 80
+Train loss: 1.412e-01
+Validation loss: 1.590e-01
+++++++++++++++++++++++++++++++++++++++++++++++++++
+Epoch: 100
+Train loss: 1.817e-01
+Validation loss: 1.366e-01
+'''
