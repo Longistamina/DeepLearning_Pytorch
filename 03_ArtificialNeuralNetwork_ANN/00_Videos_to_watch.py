8220e55fbbdb9a1e5ea7851182c2c09882cea8d5
LongSNU <longdpt1002@snu.ac.kr>
2025-12-30 11:42:31 +0900

    updated contents

---
+++ 03_ArtificialNeuralNetwork_ANN/00_Videos_to_watch.py
@@ -0,0 +1,9 @@
+'''
+PLEASE WATCH BELOW VIDEOS BEFORE GOING TO CODING FILES:
+
+1. Essential Main ideas of Neural Networks:
+https://www.youtube.com/watch?v=CqOfi41LfDw&list=PLblh5JKOoLUIxGDQs4LFFD--41Vzf-ME1&index=2
+
+2. The chain rule:
+https://www.youtube.com/watch?v=wl1myxrtQHQ&list=PLblh5JKOoLUIxGDQs4LFFD--41Vzf-ME1&index=3
+'''
