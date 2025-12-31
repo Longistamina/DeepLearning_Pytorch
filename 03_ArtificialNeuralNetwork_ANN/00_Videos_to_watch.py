6ca99b4c73865adaffcf24db086e6a12c81852c2
LongSNU <longdpt1002@snu.ac.kr>
2025-12-31 11:03:39 +0900

    updated contents

--- 03_ArtificialNeuralNetwork_ANN/00_Videos_to_watch.py
+++ 03_ArtificialNeuralNetwork_ANN/00_Videos_to_watch.py
@@ -6,4 +6,10 @@ https://www.youtube.com/watch?v=CqOfi41LfDw&list=PLblh5JKOoLUIxGDQs4LFFD--41Vzf-
 
 2. The chain rule:
 https://www.youtube.com/watch?v=wl1myxrtQHQ&list=PLblh5JKOoLUIxGDQs4LFFD--41Vzf-ME1&index=3
+
+3. Gradient Descent:
+https://www.youtube.com/watch?v=sDv4f4s2SB8&list=PLblh5JKOoLUIxGDQs4LFFD--41Vzf-ME1&index=5
+
+4. Stochastic Gradient Descent:
+https://www.youtube.com/watch?v=vMh0zPT0tLI
 '''
