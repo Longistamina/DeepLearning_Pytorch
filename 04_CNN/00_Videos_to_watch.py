5b69234716b7be28a2577f2fd26e7b3ffb530af5
Longistamina <dophamthelong03@gmail.com>
2026-01-02 16:58:21 +0900

    updated contents

--- 04_CNN/00_Videos_to_watch.py
+++ 04_CNN/00_Videos_to_watch.py
@@ -2,7 +2,7 @@
 PLEASE WATCH BELOW VIDEOS BEFORE GOING TO CODING FILES:
 
 CNN has many variants:
-LeNet -> AlexNet -> VGGNet -> ResNet -> ResNet
+LeNet -> AlexNet -> VGGNet -> ResNet -> UNet
 
 1. StatQuest explanation:
 https://www.youtube.com/watch?v=HGwBXDKFk9I&list=PLblh5JKOoLUIxGDQs4LFFD--41Vzf-ME1&index=15
