99547db86460215189982bbb6592f3ab62c9dd25
LongSNU <longdpt1002@snu.ac.kr>
2026-01-13 11:59:42 +0900

    updated contents

--- 05_GNN/learning_materials/Tutorial_02_review.py
+++ 05_GNN/learning_materials/Tutorial_02_review.py
@@ -1,4 +1,12 @@
 '''
+Watch these videos:
 + Node Classification: https://www.youtube.com/watch?v=ex2qllcVneY&list=PLV8yxwGOxvvoNkzPfCx2i8an--Tkt7O8Z&index=4
-+ How to use features in GNN: https://www.youtube.com/watch?v=mdWQYYapvR8&list=PLV8yxwGOxvvoNkzPfCx2i8an--Tkt7O8Z&index=5
++ How to use edge features in GNN: https://www.youtube.com/watch?v=mdWQYYapvR8&list=PLV8yxwGOxvvoNkzPfCx2i8an--Tkt7O8Z&index=5
+
+##################
+
+Knowledge Graphs and Node Classification:
+# We have one large graph and not many individual graphs (like molecules)
+# We infere on unlabeled nodes in this large graph and hence perform node-level predictions 
+--> We have to use different nodes of the graph depending on what we want to do
 '''
